055d5ee4ea602a86c81c8cca6e291351d4bda2be
Ilyess <rachedi.ilyess@gmail.com>
2025-05-28 17:09:08 +0200

    redesign page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { useState } from 'react';
 import './App.css';
 import Option from './components/Option';
 import GameResult from './components/GameResult';
+import { Stack, Typography } from '@mui/material';
 
 export type OptionType = "rock" | "paper" | "scissors";
 
 type OptionData = {
   value: OptionType;
-  img: string;
+  emoji: string;
+  backgroundColor: string;
 };
 
 const OPTIONS: OptionData[] = [
-  { value: "rock", img: "/rock.png" },
-  { value: "paper", img: "/paper.png" },
-  { value: "scissors", img: "/scissors.png" },
+  { value: "rock", emoji: "👊", backgroundColor: "var(--color-rock-bg)" },
+  { value: "paper", emoji: "🖐️", backgroundColor: "var(--color-paper-bg)" },
+  { value: "scissors", emoji: "✌️", backgroundColor: "var(--color-scissors-bg)" },
 ];
 
 function App() {
@@ -60,30 +62,26 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>Rock Paper Scissors</h1>
-      <p className="subtitle">Choose your option and try to beat the computer!</p>
+
+      <Typography variant="h1" sx={{ mb: 4 }}>Rock Paper Scissors</Typography>
 
       <GameResult userScore={scores.player} computerScore={scores.computer} handleReset={handleResetGame} />
-      <div className="options">
-        {OPTIONS.map((option) => (
-          <Option
-            key={option.value}
-            value={option.value}
-            img={option.img}
-            onClick={() => handleClickOption(option.value)}
-          />
-        ))}
-      </div>
-
-
-
-      <div className="results">
-        {resultMessage && <p className={`result-message`}>{resultMessage}</p>}
-      </div>
-
-      <button className="reset-button" onClick={handleResetGame}>
-        Reset Game
-      </button>
+      <Stack direction="column" alignItems="center" justifyContent="center" sx={{ my: 4 }} width="100%">
+        <Stack direction="row" alignItems="center" justifyContent="center" sx={{ mb: 2, gap: 2 }} width="100%">
+          {OPTIONS.map((option) => (
+            <Option
+              value={option.value}
+              emoji={option.emoji}
+              backgroundColor={option.backgroundColor}
+              onClick={() => handleClickOption(option.value)}
+            />
+          ))}
+        </Stack>
+        <Typography variant="h5" sx={{ my: 2 }}>Choose your option and try to beat the computer!</Typography>
+        <Typography variant="subtitle1" sx={{ mb: 3 }}>
+          Rock beats Scissors • Paper beats Rock • Scissors beats Paper
+        </Typography>
+      </Stack>
     </div>
   );
 }
