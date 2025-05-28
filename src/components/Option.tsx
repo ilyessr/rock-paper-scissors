092ee26ae8af0ab0308301754c826a9041cd7a20
Ilyess <rachedi.ilyess@gmail.com>
2025-05-28 17:09:16 +0200

    redesign option

---
+++ src/components/Option.tsx
@@ -0,0 +1,23 @@
+import { Typography, Stack } from "@mui/material";
+import { OptionType } from "../App";
+import './Option.css';
+
+interface OptionProps {
+    value: OptionType;
+    emoji: string;
+    onClick: (value: OptionType) => void;
+    size?: string;
+    backgroundColor?: string;
+}
+
+const Option = ({ value, emoji, onClick, size = "4rem", backgroundColor = "transparent", }: OptionProps) => {
+    return (
+        <Stack className="option-button" sx={{ backgroundColor: backgroundColor, borderRadius: "4px", padding: '2rem', display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', flex: 1 }} onClick={() => onClick(value)}>
+            <Typography sx={{ fontSize: size }}>
+                {emoji}
+            </Typography>
+        </Stack>
+    )
+};
+
+export default Option;
